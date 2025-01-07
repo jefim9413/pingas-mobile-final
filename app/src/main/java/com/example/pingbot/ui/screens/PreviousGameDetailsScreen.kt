d4874cca992cbe61a68281951d1b3f0c46a9b7e5
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-01-06 23:03:05 -0300

    Tela para detalhes de jogos anteriores

---
+++ app/src/main/java/com/example/pingbot/ui/screens/PreviousGameDetailsScreen.kt
@@ -0,0 +1,141 @@
+package com.example.pingbot.ui.screens
+
+import TopAppBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.pingbot.data.datasource.Game
+import com.example.pingbot.ui.components.CurrentMatch
+import com.example.pingbot.ui.components.DrawerMenu
+import com.example.pingbot.ui.components.PlayerProfile
+import kotlinx.coroutines.launch
+
+@Composable
+fun PreviousGameDetailsScreen(game: Game? = null, navController: NavHostController) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    DrawerMenu(
+        navController = navController,
+        scope = scope,
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    navController = navController,
+                    onOpenDrawer = { scope.launch { drawerState.open() } },
+                    showBackButton = true
+                )
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .background(Color(0xFFEAEAEA))
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+
+                if (game != null) {
+                    CurrentMatch(game = game, navController = navController)
+                } else {
+                    Text(
+                        text = "Informações do jogo não disponíveis",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.Gray,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+
+                Text(
+                    text = "Jogadores",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF3D66CC),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        if (game != null) {
+                            PlayerProfile(player = game.player1)
+                        } else {
+                            Text(
+                                text = "Jogador 1 não disponível",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = Color.Gray
+                            )
+                        }
+                    }
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        if (game != null) {
+
+                            if (game.winner == game.player2) {
+
+                            } else {
+                                PlayerProfile(player = game.player2)
+
+                            }
+
+                        } else {
+                            Text(
+                                text = "Jogador 2 não disponível",
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+

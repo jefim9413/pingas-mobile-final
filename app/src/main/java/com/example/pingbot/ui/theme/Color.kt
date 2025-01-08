a6216b714dc2261932430aa9c6e3e450b63d8b6c
Raphael Garcia <raphaelgarcia0607@gmail.com>
2025-01-08 03:32:53 -0300

    Paleta de cores

---
+++ app/src/main/java/com/example/pingbot/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.pingbot.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF03A9F4)
+val SecondaryColor = Color(0xFF0288D1)
+val BackgroundColor = Color(0xFF03A9F4)
+//val SurfaceColor = Color.White
+val TextColorPrimary = Color.White
+val ErrorColor = Color.Red

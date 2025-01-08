02fdce82d0ce9dc4d21581014791bd44940256ef
Raphael Garcia <raphaelgarcia0607@gmail.com>
2025-01-08 03:30:44 -0300

    Perfil do jogador

---
+++ app/src/main/java/com/example/pingbot/ui/components/PlayerProfile.kt
@@ -0,0 +1,51 @@
+package com.example.pingbot.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.pingbot.data.datasource.User
+
+
+@Composable
+fun PlayerProfile(
+    player: User,
+    size : Int? = 50,
+    showName: Boolean? = true,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (size != null) {
+            AsyncImage(
+                model = player.profileImage,
+                contentDescription = "Imagem de ${player.name}",
+                modifier = Modifier
+                    .size(size.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        if (showName == true) {
+            Text(
+                text = player.name,
+                fontSize = 24.sp,
+                color = Color.White,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}

c403dc13fa64d106e2e8aad8a15c5086fbffabdd
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-01-26 15:05:06 -0300

    Adiciona lógica de gerenciamento de partidas e botão para trocar jogadores na HomeScreen

--- app/src/main/java/com/example/pingbot/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pingbot/ui/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.pingbot.ui.screens
 
 import TopAppBar
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,30 +11,51 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.pingbot.R
+import com.example.pingbot.data.datasource.Game
+import com.example.pingbot.data.model.GameManager
 import com.example.pingbot.ui.components.BottomBar
 import com.example.pingbot.ui.components.DrawerMenu
 import com.example.pingbot.ui.components.GameScore
 import com.example.pingbot.ui.theme.BackgroundColor
+import com.example.pingbot.utils.sendNotificationToPlayers
 import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
-    navController: NavHostController
+    navController: NavHostController,
 ) {
-
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    val gameManager = GameManager()
+
+    val isLoading = remember { mutableStateOf(true) }
+    val gameState = remember { mutableStateOf<Game?>(null) }
+
+
+
+    val currentMatch = gameManager.getCurrentMatch()
+    gameState.value = currentMatch
+
 
     DrawerMenu(
         navController = navController,
@@ -48,12 +70,8 @@ fun HomeScreen(
                     showBackButton = false
                 )
             },
-
-            bottomBar = {
-                BottomBar(navController)
-            }
+            bottomBar = { BottomBar(navController) }
         ) { paddingValues ->
-            // Tela Principal
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -64,8 +82,12 @@ fun HomeScreen(
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-
-                    GameScore()
+                    // Exibe os dados do jogo atual
+                    gameState.value.let { game ->
+                        if (game != null) {
+                            GameScore(game)
+                        }
+                    }
 
                     Spacer(modifier = Modifier.height(32.dp))
 
@@ -75,8 +97,40 @@ fun HomeScreen(
                         modifier = Modifier.size(300.dp)
                     )
 
+                    Button(
+                        onClick = {
+
+                            val updatedGame = gameManager.updateCurrentMatchPlayer(
+                                isPlayer1 = true
+                            )
+
+                            if (updatedGame != null) {
+                                sendNotificationToPlayers(
+                                    context = context,
+                                    oldGame = currentMatch,
+                                    newGame = updatedGame
+                                )
+                            }
+
+                            gameState.value = updatedGame
+
+
+                            Log.d(
+                                "HomeScreen",
+                                "Jogando: ${gameState.value?.player1?.name} e ${gameState.value?.player2?.name}"
+                            )
+                        },
+                        modifier = Modifier.padding(top = 4.dp),
+                        shape = MaterialTheme.shapes.medium,
+                    ) {
+                        Text(text = "Trocar Player", color = Color.White)
+                    }
                 }
             }
         }
     }
 }
+
+
+
+
